0bb13a0678b4326c3631dcc5c0707539d6496d61
Ankit Pati <ankitpati@gmail.com>
2017-03-04 16:23:40 +0530

    C++: compile as C++11

--- filedefs/filetypes.cpp
+++ filedefs/filetypes.cpp
@@ -1,7 +1,10 @@
 [build-menu]
 FT_01_LB=_Build
-FT_01_CM=g++ -Wall -Wextra -o "%e" "%f"
+FT_01_CM=g++ -std=c++11 -Wall -Wextra -o "%e" "%f"
 FT_01_WD=
 EX_01_LB=Stylise
 EX_01_CM=indent -kr "%f"; astyle --style=kr --indent=spaces=4 -p "%f"; rm "%f.orig" "%f~"
 EX_01_WD=
+FT_00_LB=_Compile
+FT_00_CM=g++ -std=c++11 -Wall -Wextra -c "%f"
+FT_00_WD=
