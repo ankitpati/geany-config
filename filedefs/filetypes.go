4656358b1ed5acd0888654d541d630e63156c30f
Ankit Pati <ankitpati@gmail.com>
2017-04-16 17:24:54 +0530

    compile and run Go code

---
+++ filedefs/filetypes.go
@@ -0,0 +1,13 @@
+[build-menu]
+FT_00_LB=_Build
+FT_00_CM=go build "%f"
+FT_00_WD=
+FT_01_LB=Compile
+FT_01_CM=go build "%f"
+FT_01_WD=
+EX_00_LB=Execute
+EX_00_CM="./%e"
+EX_00_WD=
+EX_01_LB=Stylise
+EX_01_CM=go fmt "%f"
+EX_01_WD=
