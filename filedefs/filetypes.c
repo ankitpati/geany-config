7e1e224ddd995c70b8f186559a6d758236721fd6
Ankit Pati <ankitpati@gmail.com>
2017-05-31 22:44:14 +0530

    C/C++: stricter warnings, better optimisation

--- filedefs/filetypes.c
+++ filedefs/filetypes.c
@@ -1,9 +1,9 @@
 [build-menu]
 FT_01_LB=_Build
-FT_01_CM=gcc -Wall -Wextra -o "%e" "%f" -lm
+FT_01_CM=gcc -Wall -Wextra -Wpedantic -O3 -o "%e" "%f" -lm && strip "%e"
 FT_01_WD=
 FT_00_LB=_Compile
-FT_00_CM=gcc -Wall -Wextra -c "%f"
+FT_00_CM=gcc -Wall -Wextra -Wpedantic -c "%f"
 FT_00_WD=
 EX_01_LB=Stylise
 EX_01_CM=indent -kr "%f"; astyle --style=kr --indent=spaces=4 -p "%f"; rm "%f.orig" "%f~"
