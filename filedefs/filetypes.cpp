1554f11ee84955778f65d43b04b1cafea13c46e2
Ankit Pati <contact@ankitpati.in>
2017-08-03 08:30:07 +0530

    BGI graphics support added

--- filedefs/filetypes.cpp
+++ filedefs/filetypes.cpp
@@ -1,10 +1,10 @@
 [build-menu]
 FT_01_LB=_Build
-FT_01_CM=g++ -std=c++11 -Wall -Wextra -O3 -o "%e" "%f" && strip "%e"
+FT_01_CM=g++ -std=c++11 -Wall -Wextra -Wpedantic -Wno-unused-result -O3 -o "%e" "%f" -lSDL_bgi -lSDL2 && strip "%e"
 FT_01_WD=
 EX_01_LB=Stylise
 EX_01_CM=indent -kr "%f"; astyle --style=kr --indent=spaces=4 -p "%f"; rm "%f.orig" "%f~"
 EX_01_WD=
 FT_00_LB=_Compile
-FT_00_CM=g++ -std=c++11 -Wall -Wextra -Wpedantic -c "%f"
+FT_00_CM=g++ -std=c++11 -Wall -Wextra -Wpedantic -Wno-unused-result -c "%f"
 FT_00_WD=
